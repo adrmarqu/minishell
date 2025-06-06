6868f36958eed464cb33f7a55a82b66fb2559282
Adrià Márquez Nebot <adrmarqu@cbr6s1.42barcelona.com>
2025-06-06 20:34:23 +0200

    Expanded variables, but quots se mantienen

--- srcs/parser/expand.c
+++ srcs/parser/expand.c
@@ -6,14 +6,14 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 14:06:43 by adrmarqu          #+#    #+#             */
-/*   Updated: 2025/06/06 20:23:43 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2025/06/06 20:32:30 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parser.h"
 #include "../../libft/libft.h"
 
-char	*expand_var(char *str, int *idx, t_data *data)
+static char	*expand_var(char *str, int *idx, t_data *data)
 {
 	char	**split;
 	char	*ret;
@@ -32,7 +32,7 @@ char	*expand_var(char *str, int *idx, t_data *data)
 	return (ret);
 }
 
-static char	*expand_simple(char *str, int *init, char c)
+char	*expand_simple(char *str, int *init, char c)
 {
 	int		i;
 	char	*ret;
@@ -56,9 +56,10 @@ static char	*expand_simple(char *str, int *init, char c)
 	return (ret);
 }
 
-static char	*expand_double(char *str, int *init, t_data *data)
+char	*expand_double(char *str, int *init, t_data *data)
 {
 	int		i;
+	char	*ret;
 
 	i = *init + 1;
 	while (str[i] && str[i] != '\"')
@@ -73,25 +74,29 @@ static char	*expand_double(char *str, int *init, t_data *data)
 		}
 		i++;
 	}
-	return (expand_simple(str, init, '\"'));
+	ret = ft_strdup(str);
+	free(str);
+	return (ret);
 }
 
-static char	*expand_input(char *str, t_data *data)
+static char	*expand_vars(char *str, t_data *data)
 {
 	int		i;
 	int		len;
+	bool	simple;
 
 	i = 0;
+	simple = false;
 	while (i < len)
 	{
 		if (str[i] == '\'')
-			str = expand_simple(str, &i, '\'');
-		else if (str[i] == '\"')
-			str = expand_double(str, &i, data);
-		else if (str[i] == '$')
+			simple = !simple;
+		else if (str[i] == '$' && !simple)
+		{
 			str = expand_var(str, &i, data);
-		if (!str)
-			return (NULL);
+			if (!str)
+				return (NULL);
+		}
 		i++;
 		len = ft_strlen(str);
 	}
@@ -108,13 +113,19 @@ int	expand(t_token **tokens, t_data *data)
 	first = *tokens;
 	while (*tokens)
 	{
-		tmp = expand_input(ft_strdup((*tokens)->value), data);
+		tmp = expand_vars(ft_strdup((*tokens)->value), data);
 		if (!tmp)
 			return (1);
+
+		// Expandir *
+
 		//new_str = expand_card(tmp);
 		//free(tmp);
 		//if (!new_str)
 		//	return (1);
+
+		// Quitar comillas
+
 		free((*tokens)->value);
 		//(*tokens)->value = new_str;
 		(*tokens)->value = tmp;
