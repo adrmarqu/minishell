a80b317d5e3afbcccb42b16ff3239ab095b55186
Adrià Márquez Nebot <adrmarqu@cbr8s2.42barcelona.com>
2025-07-06 16:00:57 +0200

    env

--- srcs/built-ins/env_mini.c
+++ srcs/built-ins/env_mini.c
@@ -6,15 +6,29 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 16:56:26 by adrmarqu          #+#    #+#             */
-/*   Updated: 2025/07/04 19:51:13 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2025/07/06 16:00:16 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/global.h"
+#include "../../libft/libft.h"
 
 int	blt_env(t_data *data, t_token *cmd)
 {
-	(void)data;
-	(void)cmd;
+	t_env	*env;
+
+	cmd = cmd->next;
+	env = data->env;
+	while (env)
+	{
+		if (env->equal)
+		{
+			fd_printf(1, "%s=", env->var);
+			if (env->value)
+				fd_printf(1, "%s", env->value);
+			fd_printf(1, "\n");
+		}
+		env = env->next;
+	}
 	return (0);
 }
